1e2d4221672013374bb2e032af9abf0beb3b806b
ashqqy <Artiom.shaff07@gmail.com>
2024-08-21 20:25:12 +0500

    Вторая версия
    Добавлены юнит тесты (Функции UnitTest и AllUnitTests).
    
    При запуске программы можно выбрать два режима: режим тестирования или режим решения квадратных уравнений (для этого создана функция CheckOrSolve).
    
    Функция CampareWithZero усовершенствована в функцию CompareDoubles, теперь она может сравнивать double'ы между собой, а не только double'ы с нулем.
    
    Добавлена функция OutputSolveSquare, отвечающая за вывод решений квадратного уравнения (функция SolveSquare только решает уравнения, без вывода).
    
    Функция scan переделана в функцию DoubleScan, теперь она позволяет вводить одно double число с клавиатуры и обрабатывает ошибки при вводе.
    
    Также другие небольшие изменения.

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -8,54 +8,95 @@ const int ZERO_ROOT = 0;
 const int ONE_ROOT  = 1;
 const int TWO_ROOTS = 2;
 const int INF_ROOTS = -1;
+
 const double delta  = 10e-8;
+
 const double ERROR  = -100;
 
 //-----------------------------------------------------------------------------
 
+double CheckOrSolve ();
+
 int SolveSquare (double a, double b, double c, double* x1, double* x2);
-bool CampareWithZero (double num);
+int OutputSolveSquare (double x1, double x2, int numRoots);
+
+bool CompareDoubles (double num1, double num2);
+
+void DoubleScan (double* coef);
 void CleanBufer ();
-void scan (double* a, double* b, double* c);
+
+int UnitTest (int numTest, double a, double b, double c, double x1Exp, double x2Exp, int numRootsExp);
+void AllUnitTests ();
 
 //-----------------------------------------------------------------------------
 
 int main()
-{
-    printf ("Solve Square Equation by Shafeev Artiom\n\n");
-
-    double a = 0, b = 0, c = 0;
-
-    printf ("Enter the coefficients (a b c):\n");
-    scan (&a, &b, &c);
-
-    double x1 = 0, x2 = 0;
-
-    int nRoots = SolveSquare (a, b, c, &x1, &x2);
-
-    switch (nRoots)
     {
-        case ZERO_ROOT:
-            printf ("0 root\n");
-            break;
+    printf ("Solve Square Equation\n"
+            "(by Shafeev Artiom)\n\n");
 
-        case ONE_ROOT:
-            printf ("1 root: %lf\n", x1);
-            break;
+    double userChoice = CheckOrSolve ();
 
-        case TWO_ROOTS:
-            printf ("2 roots: %lf %lf\n", x1, x2);
-            break;
+    if (CompareDoubles (userChoice, 1))
+        {
+        double a = 0, b = 0, c = 0;
 
-        case INF_ROOTS:
-            printf ("Infinite roots\n");
-            break;
+        printf ("Enter the coefficient a:\n");
+        DoubleScan (&a);
+        printf ("Enter the coefficient b:\n");
+        DoubleScan (&b);
+        printf ("Enter the coefficient c:\n");
+        DoubleScan (&c);
 
-        default:
-            printf ("Warning\n");
-            break;
+        double x1 = 0, x2 = 0;
+        int numRoots = SolveSquare (a, b, c, &x1, &x2);
+        OutputSolveSquare (x1, x2, numRoots);
+        }
+
+    if (CompareDoubles (userChoice, 2))
+        {
+        AllUnitTests ();
+        }
     }
-}
+
+//-----------------------------------------------------------------------------
+
+double CheckOrSolve ()
+    {
+    double choice = 0;
+    printf ("What do you want?\n"
+            "Solve the equation - enter 1\n"
+            "Check the program  - enter 2\n"
+            "Your choice - ");
+
+    DoubleScan (&choice);
+    printf ("\n");
+
+    while (CompareDoubles(choice, 1) == 0 && CompareDoubles(choice, 2) == 0)
+        {
+        printf ("Try again: ");
+        DoubleScan (&choice);
+        }
+    return choice;
+    }
+
+//-----------------------------------------------------------------------------
+
+void DoubleScan (double* coef)
+    {
+    int NofSuccessfulCoef = scanf ("%lf", coef);
+    assert (NofSuccessfulCoef != EOF);
+
+
+    CleanBufer ();
+
+    while (NofSuccessfulCoef != 1)
+        {
+        printf ("Try again: ");
+        NofSuccessfulCoef = scanf ("%lf", coef);
+        CleanBufer ();
+        }
+    }
 
 //-----------------------------------------------------------------------------
 
@@ -65,87 +106,150 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2)
     assert (x2 != 0);
     assert (x1 != x2);
 
-    if (CampareWithZero (a) && CampareWithZero (b) && CampareWithZero (c))
-    {
+    if (CompareDoubles (a, 0) && CompareDoubles (b, 0) && CompareDoubles (c, 0))
+        {
         return INF_ROOTS;
-    }
-    if (CampareWithZero (a) && CampareWithZero (b))
-    {
+        }
+    if (CompareDoubles (a, 0) && CompareDoubles (b, 0))
+        {
         return ZERO_ROOT;
-    }
-    if (CampareWithZero (a))
-    {
+        }
+    if (CompareDoubles (a, 0))
+        {
         *x1 = -c / b;
         return ONE_ROOT;
-    }
+        }
 
-    if (CampareWithZero (a) == 0)
-    {
+    if (CompareDoubles (a, 0) == 0)
+        {
         double Discriminant = b*b - 4*a*c;
 
         if (Discriminant < 0)
-        {
+            {
             return ZERO_ROOT;
-        }
+            }
 
-        if (CampareWithZero (Discriminant))
-        {
-            *x1 = (-b + sqrt (Discriminant))/(2*a);
+        if (CompareDoubles (Discriminant, 0))
+            {
+            *x1 = -b/(2*a);
             return ONE_ROOT;
-        }
+            }
 
         if (Discriminant > 0)
-        {
-            *x1 = (-b + sqrt (Discriminant))/(2*a);
-            *x2 = (-b - sqrt (Discriminant))/(2*a);
+            {
+            *x1 = (-b - sqrt (Discriminant))/(2*a);
+            *x2 = (-b + sqrt (Discriminant))/(2*a);
             return TWO_ROOTS;
-        }
+            }
 
-    }
-        return ERROR;
+        }
+    return ERROR;
     }
 
 //-----------------------------------------------------------------------------
 
-bool CampareWithZero (double num)
-{
-    if (fabs (num) < delta)
+int OutputSolveSquare (double x1, double x2, int numRoots)
     {
+    switch (numRoots)
+        {
+        case ZERO_ROOT:
+            printf ("\nThe quadratic equation has 0 root\n\n");
+            return 0;
+
+        case ONE_ROOT:
+            printf ("\nThe quadratic equation has 1 root:\n"
+                    "This root: %lf\n\n", x1);
+            return 0;
+
+        case TWO_ROOTS:
+            printf ("\nThe quadratic equation has 2 roots:\n"
+                    "First  root: %lf\n"
+                    "Second root: %lf\n\n",
+                    x1, x2);
+            return 0;
+
+        case INF_ROOTS:
+            printf ("\nThe quadratic equation has infinite roots\n\n");
+            return 0;
+
+        default:
+            printf ("Error\n\n");
+            return 1;
+        }
+    }
+//-----------------------------------------------------------------------------
+
+int UnitTest (int numTest, double a, double b, double c, double x1Exp, double x2Exp, int numRootsExp)
+    {
+    double x1 = 0, x2 = 0;
+    int numRoots = SolveSquare (a, b, c, &x1, &x2);
+
+    if (CompareDoubles(x1, x1Exp) == 0 || CompareDoubles(x2, x2Exp) == 0  || CompareDoubles(numRoots, numRootsExp) == 0)
+        {
+        printf ("Error (Test %d):\n"
+                "a = %lg, b = %lg, c = %lg,\n"
+                "x1 = %lg, x2 = %lg, numRoots = %d\n"
+                "Expected:\n"
+                "x1 = %lg, x2 = %lg, numRoots = %d\n\n",
+                numTest, a, b, c, x1, x2, numRoots, x1Exp, x2Exp, numRootsExp);
         return 1;
+        }
+    return 0;
     }
-    else
+
+//-----------------------------------------------------------------------------
+
+void AllUnitTests ()
     {
-        return 0;
+    int TestErrorCounter = 0;
+    TestErrorCounter += UnitTest (1,  0,   1,    1,     -1, 0, ONE_ROOT);   // a = 0
+    TestErrorCounter += UnitTest (2,  1,   0,    -4,    -2, 2, TWO_ROOTS);  // b = 0
+    TestErrorCounter += UnitTest (3,  1,   1,    0,     -1, 0, TWO_ROOTS);  // c = 0
+
+    TestErrorCounter += UnitTest (4,  0,   0,    1,     0,  0, ZERO_ROOT);  // a = 0, b = 0
+    TestErrorCounter += UnitTest (5,  0,   1,    0,     0,  0, ONE_ROOT);   // a = 0, c = 0
+    TestErrorCounter += UnitTest (6,  1,   0,    0,     0,  0, ONE_ROOT);   // b = 0, c = 0
+
+    TestErrorCounter += UnitTest (7,  0,   0,    0,     0,  0, INF_ROOTS);  // a = 0, b = 0, c = 0
+
+    TestErrorCounter += UnitTest (8,  1,   2,    -3,    -3, 1, TWO_ROOTS);  // D > 0
+    TestErrorCounter += UnitTest (9,  1,   2,    1,     -1, 0, ONE_ROOT);   // D = 0
+    TestErrorCounter += UnitTest (10, 1,   1,    1,     0,  0, ZERO_ROOT);  // D < 0
+
+    TestErrorCounter += UnitTest (11, 6.6, 13.2, -19.8, -3, 1, TWO_ROOTS);  // not int coefficients
+
+    if (TestErrorCounter == 0)
+        {
+        printf ("All tests are successful\n");
+        }
+    else
+        {
+        printf ("%d test(s) are unsuccessful\n", TestErrorCounter);
+        }
     }
-}
+
+//-----------------------------------------------------------------------------
+
+bool CompareDoubles (double num1, double num2)
+    {
+    if (fabs (num1 - num2) < delta)
+        {
+        return 1;
+        }
+    return 0;
+    }
 
 //-----------------------------------------------------------------------------
 
 void CleanBufer ()
-{
-    while (getchar() != '\n')
     {
+    int LastSymb = 0;
+    while ((LastSymb = getchar ()) != '\n' && LastSymb != EOF)
+        {
+        }
     }
-}
 
 //-----------------------------------------------------------------------------
 
-void scan (double* a, double* b, double* c)
-{
-    int NofSuccessfulCoef = scanf ("%lf %lf %lf", a, b, c);
-    CleanBufer ();
-
-    if (NofSuccessfulCoef != 3)
-    {
-        while (NofSuccessfulCoef != 3)
-        {
-            printf ("Try again:\n");
-            NofSuccessfulCoef = scanf ("%lf %lf %lf", a, b, c);
-            CleanBufer ();
-        }
-    }
-}
 
 
-// сделать структуры
-// сделать свою функцию ввода (очистка буфера+проверка сканфа)
